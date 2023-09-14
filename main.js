7dbf8e1cdad5634a94d38381bedbd156ca1b1949
nancynsalazar.tech <nancy_salazar@outlook.com>
2023-09-13 20:12:26 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
+ cursor: "<span style='color: #fb4993;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('<span style="color: #fb4993;">Enseño programación web y hago contenido de tecnología.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
